22470dad8eecc4582d13c3fb2568412da8471230
elmetador2 <ahnsharik@gmail.com>
2024-09-22 07:04:36 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,4 @@
+const accountId=144553
+let accountemail="shrik@gmail.com"
+// accountId=2
+console.log(accountId);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"// treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+// console.log(3+3); console.log("sharique") //cant be used because code is not readable
+let name="sharique"
+let age=21
+let isLoggedIn=false
+// number=>2^53
+// bigint 
+// string=> prefer double quotes""
+// boolean =>true or false
+// null =>standalone value special 
+// undefinded=> value is not assigned
+// symbol=>unique
+
+// object=>disccused later
+//null is an object
+console.log(typeof"sharique);//give types of variable
