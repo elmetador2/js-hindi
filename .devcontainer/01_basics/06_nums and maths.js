fa1efe828a75c58c344e9a90f9473e223d085aba
elmetador2 <ahnsharik@gmail.com>
2024-09-22 15:23:50 +0000

    numvber

---
+++ .devcontainer/01_basics/06_nums and maths.js
@@ -0,0 +1,9 @@
+const score=400
+//output=400
+
+const balance = new Number(100)
+console.log(balance);//output=numnber:100
+
+console.log(balance.toString());//type=string =100 now it have the properties of sting also
+
+
