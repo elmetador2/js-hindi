00b21c91555e2609ea2dd9ce6d938dcf0f235906
elmetador2 <ahnsharik@gmail.com>
2024-09-22 11:09:01 +0000

    conversion type

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -19,3 +19,43 @@ console.log(booleanIsLoggedin);//output=true
 //1=>true
 //""=>false
 //sharique=>true
+
+//********************operatons ***************
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+console.log(2+2);//output=4
+console.log(2-2);//output=0
+console.log(2**2);//output=power function:2^2=4
+
+let str1="hello"
+let str2="hitesh"
+let str3= str1 + str2//output=>hello hitesh
+
+console.log("1"+2);//output=12
+console.log(1+"2");// output=12
+console.log("1"+2+2);//output=122 
+console.log(1+2 + "2");//output=>32
+//learn to primitive guideline from ECMA
+console.log(true);//output=>true
+console.log(+true);//output=1
+console.log(+"");//output=0
+
+let num1,num2, num3
+
+num1=num2=num3=2+2//not a good way
+
+let gameCounter=100
+gameCounter++;//refer mdn
+console.log(gameCounter);//output=101
+
+
+
+
+
+
+
+
+
